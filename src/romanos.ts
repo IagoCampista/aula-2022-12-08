a847b7dfb7de8b22a76889cbdfe9dcfc62c1680c
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2022-12-08 10:48:27 -0300

    teste 5 para V passando; lista de conversão implementada

--- src/romanos.ts
+++ src/romanos.ts
@@ -1,9 +1,21 @@
+
 class Romanos {
-    converte(arabicos:number):string{
-        if(arabicos == 1)
-        return "I"
-        else
-        return "X"
+    private arabicos = [
+        1,
+        5,
+        10
+    ]
+    
+    private romanos = [
+        'I',
+        'V',
+        'X'
+    ]
+
+    converte(arabico: number):string{
+        const indice = this.arabicos.indexOf(arabico);
+        if(indice == -1) throw new Error("arabico desconhecido");
+        else return this.romanos[indice];
     } 
 }
 
