eee86f76b6e8578af8e5dee3677521f02f0f35bf
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2022-12-08 10:39:32 -0300

    teste 5 para v falhando

--- src/romanos.test.ts
+++ src/romanos.test.ts
@@ -21,4 +21,13 @@ describe('romanos', () => {
         expect (numeroRomanos).toEqual("X");
     });
 
+    it('deveria converter 5 para "V"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(5); 
+        //expectation
+        expect (numeroRomanos).toEqual("V");
+    });
+
 })
