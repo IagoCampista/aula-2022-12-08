6a7645b846c10febfee84ddce8d33b6325378030
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-12-16 23:28:40 -0300

    add link para o repo como comentario

--- src/romanos.ts
+++ src/romanos.ts
@@ -50,25 +50,5 @@ class Romanos {
 
 export {Romanos}
 
+//Link para o repositorio: https://github.com/IagoCampista/aula-2022-12-08.git
 
-/*
-converte(arabico: number):string{ 
-        const posicao_encontrada = this.arabicos.indexOf(arabico);
-        if(posicao_encontrada >= 0){
-            return this.romanos[posicao_encontrada];
-        } else {
-            const posicao_encontrada2 = this.arabicos.indexOf(arabico-1);
-            if (posicao_encontrada2 >= 0) {
-                // ok
-                return this.romanos[posicao_encontrada2]+"I";
-            } else {
-                const posicao_encontrada3 = this.arabicos.indexOf(arabico-2);
-                if (posicao_encontrada3 >= 0) {
-                    // ok
-                    return this.romanos[posicao_encontrada3]+"II";
-                }
-            }
-        }
-        throw new Error("arabico desconhecido");
-    } 
-*/
