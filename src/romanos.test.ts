ca1997b7ad1c2f228be1c632928e97b2e2edd6ef
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-12-16 23:24:54 -0300

    Novo approach testado e nao commitado o passo a passo
    A ideia é ir diminuindo o numero dos algarismos ja formados ate chegar a 0
    
    o babystep nao foi seguido tao a risca por nao saber um passo intermediario antes do passo final para esse problema

--- src/romanos.test.ts
+++ src/romanos.test.ts
@@ -22,6 +22,52 @@ it('deveria converter 10 para "X"', () => {
         expect (numeroRomanos).toEqual("X");
     });
 
+it('deveria converter 11 para "XI"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(11);
+        //expectation
+        expect (numeroRomanos).toEqual("XI");
+    });
+
+    it('deveria converter 20 para "XX"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(20);
+        //expectation
+        expect (numeroRomanos).toEqual("XX");
+    });
+
+    it('deveria converter 430 para "CDXXX"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(430);
+        //expectation
+        expect (numeroRomanos).toEqual("CDXXX");
+    });
+
+    it('deveria converter 432 para "CDXXXII"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(432);
+        //expectation
+        expect (numeroRomanos).toEqual("CDXXXII");
+    });
+    it('deveria converter 1967 para "MCMLXVII"', () => {
+        //setup
+        const sut = new Romanos();
+        //action
+        const numeroRomanos = sut.converte(1967);
+        //expectation
+        expect (numeroRomanos).toEqual("MCMLXVII");
+    });
+
+
+
 
 
 
--- src/romanos.ts
+++ src/romanos.ts
@@ -1,55 +1,50 @@
 
 class Romanos {
     private arabicos = [
-        1,
-        4,
-        5,
-        9,
-        10,
-        40,
-        50,
-        90,
-        100,
-        400,
-        500,
+        1000,
         900,
-        1000
+        500,
+        400,
+        100,
+        90,
+        50,
+        40,
+        10,
+        9,
+        5,
+        4,
+        1             
     ]
     
     private romanos = [
-        'I',
-        'IV',
-        'V',
-        'IX',
-        'X',
-        'XL',
-        'L',
-        'XC',
-        'C',
-        'CD',
-        'D',
+        'M',
         'CM',
-        'M'
+        'D',
+        'CD', 
+        'C',
+        'XC',
+        'L',
+        'XL',
+        'X',
+        'IX',
+        'V',
+        'IV',
+        'I'        
     ]
 
     converte(arabico: number):string{ 
+        let num = arabico;
         let result = '';
         let i=0;
-        for (i=0; i<14; i++){
-            if(arabico==this.arabicos[i]){
-                // i=0;
-                result = result +this.romanos[i]
+        for (i=0; i<this.arabicos.length; i++){
+            if(num>=this.arabicos[i]){
+                num = num - this.arabicos[i];
+                result = result +this.romanos[i];
+                i=0;
             }
             
         }
         return result;
-        // const posicao_encontrada = this.arabicos.indexOf(arabico);
-        // if(posicao_encontrada >= 0){
-        //     return this.romanos[posicao_encontrada]
-        // } else {
-        //     return 'hj nao'
-        // }
-        // throw new Error("arabico desconhecido");
     } 
 }
 
